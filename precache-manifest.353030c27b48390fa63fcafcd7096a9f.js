64bf6efc5ab4e563591a873c1cd00bac9ca1a088
ShuvaDev <s.r.shuvadeb@gmail.com>
2020-05-25 15:40:16 +0600

    Updates

---
+++ precache-manifest.353030c27b48390fa63fcafcd7096a9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ccaa4298930fd70488689e5835f3ab",
+    "url": "/React-Stopwatch/index.html"
+  },
+  {
+    "revision": "6858dd43f9db192c3b2a",
+    "url": "/React-Stopwatch/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "89799f8a59afba4e7f3f",
+    "url": "/React-Stopwatch/static/css/main.1f79e56a.chunk.css"
+  },
+  {
+    "revision": "6858dd43f9db192c3b2a",
+    "url": "/React-Stopwatch/static/js/2.221c2842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Stopwatch/static/js/2.221c2842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89799f8a59afba4e7f3f",
+    "url": "/React-Stopwatch/static/js/main.cc5e6a84.chunk.js"
+  },
+  {
+    "revision": "1b0200fc919f98bdd11d",
+    "url": "/React-Stopwatch/static/js/runtime-main.a49ce9ab.js"
+  }
+]);
